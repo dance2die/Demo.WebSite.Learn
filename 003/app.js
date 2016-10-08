fb566d0a2904190b0f6184b7ebc4af8448f6e727
dance2die <dontbothermewithspam@gmail.com>
2016-10-08 13:44:18 -0400

    Trying out a few Parallax.JS options

--- 003/app.js
+++ 003/app.js
@@ -1,6 +1,11 @@
 $(document).ready(function (){
-    $('#scene1').parallax();
-    $('#scene2').parallax();
+    $('#scene1').parallax({
+        calibrateY: true
+    });
+    $('#scene2').parallax({
+        calibrateY: false,
+        limitY: 0
+    });
 });
 
 
